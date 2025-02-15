3d5e0fb2309eeb02bf0e3e5ada729a005c788ece
idfyr <69794427+idfyr@users.noreply.github.com>
2025-02-15 20:43:32 +0700

    Update config.fish

--- config.fish
+++ config.fish
@@ -6,7 +6,21 @@ set -Ux fish_greeting
 set -Ux LANG "en_US.utf-8"
 set -Ux  LC_ALL "en_US.UTF-8"
 
-alias ls='lsd'
-
 starship init fish | source
+
+# Add newline after prompt
+function starship_transient_prompt_func
+	tput cuu1
+	starship module character
+end
+function prompt_newline --on-event fish_postexec
+	echo
+end
+alias clear "command clear; commandline -f clear-screen"
+
+zoxide init fish | source
+
+alias ls='lsd'
+alias cd='z'
+
 ~/.local/bin/mise activate fish | source
